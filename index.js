cffd7b3ddb7fd620829e85b91bf6d1c3069539a7
Brendan Le Galo <le.galo.brendan@gmail.com>
2023-10-11 11:28:00 +0200

    update of the code now we can visualize on the button a X if there is a mine or not. if there are no mine we can see how many mines that surround the button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Variable initialisation for every location or button
 var b00=0; var b01=0; var b02=0; var b03=0; var b04=0; var b05=0; var b06=0; var b07=0; var b08=0; var b09=0;
 var b10=0; var b11=0; var b12=0; var b13=0; var b14=0; var b15=0; var b16=0; var b17=0; var b18=0; var b19=0;
 var b20=0; var b21=0; var b22=0; var b23=0; var b24=0; var b25=0; var b26=0; var b27=0; var b28=0; var b29=0;
@@ -9,9 +10,16 @@ var b70=0; var b71=0; var b72=0; var b73=0; var b74=0; var b75=0; var b76=0; var
 var b80=0; var b81=0; var b82=0; var b83=0; var b84=0; var b85=0; var b86=0; var b87=0; var b88=0; var b89=0;
 var b90=0; var b91=0; var b92=0; var b93=0; var b94=0; var b95=0; var b96=0; var b97=0; var b98=0; var b99=0;
 
+/****************** Determination of the mines spots *********************/
 
-var locations = []; // var that will contain all the possible location (in our situation it will be all numbers from 0 to 99)
-var minesLocation = []; // var that will contain the mine location that has been randomly placed.
+/* that section is aiming to determiate where the mines should be placed on a separated array, they are not assigned to their button yet
+(ex: var b12 will still be =0)
+The determination of the mines spot are done randomly thanks to the function "placeMine(numberOfMines)"-->(we can chose the number of mines we want)
+It picks randomly a mine spot among all the available spot (like if we were picking a card from the deck) then it removed the taken spot from the
+available spot for the next selection  */
+
+var locations = []; // var that will contain all the possible mine location (in our situation it will be an array with every numbers from 0 to 99)
+var minesLocation = []; // var that will contain the mine location that has been randomly picked.
 
 function placeMine (numberOfMines) { 
     // Loop that generate all the different possible location that we will be able to choose from (we can change the number of locations by changing i)
@@ -21,7 +29,7 @@ function placeMine (numberOfMines) {
     //Loop that pick one random location from the available location array. start again until we have the right number of mines.
     for (var i=0; i<numberOfMines; i++){
         var n = Math.floor(Math.random()*locations.length); // n is the position in the array (not the location itself)
-        minesLocation.push(locations.splice(n,1));//it remove the taken location from the location matrix so that we can't pick two time the same locaation
+        minesLocation.push(locations.splice(n,1));//it remove the taken location from the "location" array so that we can't pick two time the same locaation
     } 
 
     return locations;// what's left from the different locations - mine free spots
@@ -32,9 +40,11 @@ placeMine(10);
 console.log(locations); 
 console.log(minesLocation); 
 
+/************** Assignement of the mines to their button *****************/
+
 var numMines=minesLocation.length;
 var singleMineLocation;
-for (var i=0; i<numMines; i++){ // assigne the value 1 to the variable that has recieve a bomb (ex: if the var b02 has recieve a bomb b02=1)
+for (var i=0; i<numMines; i++){ // the loop assigne the value 1 to the variable that has recieve a bomb (ex: if the var b02 has recieve a bomb b02=1)
     singleMineLocation =minesLocation.shift().toString();
     if (singleMineLocation<10) {
         eval("b0"+singleMineLocation+"="+"1");
@@ -43,29 +53,76 @@ for (var i=0; i<numMines; i++){ // assigne the value 1 to the variable that has 
     }
 }
 
-var bomba;
-var MineOrNot;
-var c;
-var l;
+/************** Count the number of mine around each button ******************/
+
+var bomba; // this variable is the key value assign to each button, it shows a X if there is a mine, if there are none it shows the number of mines that surrounds the button
+var MineOrNot; // for the loop it will take every spot one by one and check if there is a mine or not
+var idSpot; // the variable will take out the name of the spot one by one according the position of the loop ex: b24
+var l; //  the line number of the idSpot ex: if idSpot=b24 --> l=2;
+var c; //  the column number of the idSpot ex: if idSpot=b24 --> c=4;
+var idA; var idB; var idC; var idD; var idE; var idF; var idG; var idH; // value of all variables that surrounds one spots
+/*
+idA    idB    idC
+idD  idSpot   idE
+idF    idG    idH
+*/
+//functions that count the value around the idSpot
+function countA (l,c){
+    if ((l-1)<0 || (c-1)<0) {idA = 0;} else {idA = eval("b"+(l-1)+(c-1));}
+    return idA;
+}
+function countB (l,c){
+    if ((l-1)<0) {idB = 0;} else {idB = eval("b"+(l-1)+(c));}
+    return idB;
+}
+function countC (l,c){
+    if ((l-1)<0 || (c+1)>9) {idC = 0;} else {idC = eval("b"+(l-1)+(c+1));}
+    return idC;
+}
+function countD (l,c){
+    if ((c-1)<0) {idD = 0;} else {idD = eval("b"+(l)+(c-1));}
+    return idD;
+}
+function countE (l,c){
+    if ((c+1)>9) {idE = 0;} else {idE = eval("b"+(l)+(c+1));}
+    return idE;
+}
+function countF (l,c){
+    if ((l+1)>9 || (c-1)<0) {idF = 0;} else {idF = eval("b"+(l+1)+(c-1));}
+    return idF;
+}
+function countG (l,c){
+    if ((l+1)>9) {idG = 0;} else {idG = eval("b"+(l+1)+(c));}
+    return idG;
+}
+function countH (l,c){
+    if ((l+1)>9 || (c+1)>9) {idH = 0;} else {idH = eval("b"+(l+1)+(c+1));}
+    return idH;
+}
 
 for (var i=0; i<100; i++){ // to display where are the mine on the button
     if (i<10){
         MineOrNot=eval("b0"+i); //analyse one by one (with the loop) every location (var b00, b01, b02...) to see if there is a mine or not (0 or 1).
         if (MineOrNot===1){ // if there is a mine
             bomba="X";      // place a X
-        } else {
-            bomba=".";      // if not place a . 
-            // ici il faut compter le nombre de bombe qu'il y a autour !!! réussir à isoler "l" et "c" pour pouvoir faire l'analyse.
-            // toutes les bombes sont déjà placé dans les b00, b01, b02... 
-            // avoir une variable qui prend le nom de la variable en chaine de char ex : emplacement = "b02" pour issoler le 0 et le 2  
-
+        } else { // if not show the number of mines around
+            idSpot="b0"+i; // string showing for ex b02
+            l=Number(idSpot.slice(1,2)); //num for ex: 0
+            c=Number(idSpot.slice(2,3)); //num for ex: 2
+            
+            bomba=countA(l,c)+countB(l,c)+countC(l,c)+countD(l,c)+countE(l,c)+countF(l,c)+countG(l,c)+countH(l,c);
         }
-    } else { 
+    } else { //if i>=10
         MineOrNot=eval("b"+i);
         if (MineOrNot===1){
             bomba="X";
         } else {
-            bomba=".";
+            idSpot="b"+i; // string showing for ex b32
+            l=Number(idSpot.slice(1,2)); //num for ex: 3
+            c=Number(idSpot.slice(2,3)); //num for ex: 2
+
+            // bomba=countE(l,c);
+            bomba=countA(l,c)+countB(l,c)+countC(l,c)+countD(l,c)+countE(l,c)+countF(l,c)+countG(l,c)+countH(l,c);
         }
     }
 document.querySelectorAll("button")[i].textContent=bomba; // display the X or the . for every button
@@ -75,27 +132,27 @@ document.querySelectorAll("button")[i].textContent=bomba; // display the X or th
 
 
 
-console.log(numMines);
-console.log("b00="+b00);
-console.log("b01="+b01);
-console.log("b02="+b02);
-console.log("b03="+b03);
-console.log("b04="+b04);
-console.log("b05="+b05);
-console.log("b06="+b06);
-console.log("b07="+b07);
-console.log("b08="+b08);
-console.log("b09="+b09);
-console.log("b10="+b10);
-console.log("b11="+b11);
-console.log("b12="+b12);
-console.log("b13="+b13);
-console.log("b14="+b14);
-console.log("b15="+b15);
-console.log("b16="+b16);
-console.log("b17="+b17);
-console.log("b18="+b18);
-console.log("b19="+b19);
+// console.log(numMines);
+// console.log("b00="+b00);
+// console.log("b01="+b01);
+// console.log("b02="+b02);
+// console.log("b03="+b03);
+// console.log("b04="+b04);
+// console.log("b05="+b05);
+// console.log("b06="+b06);
+// console.log("b07="+b07);
+// console.log("b08="+b08);
+// console.log("b09="+b09);
+// console.log("b10="+b10);
+// console.log("b11="+b11);
+// console.log("b12="+b12);
+// console.log("b13="+b13);
+// console.log("b14="+b14);
+// console.log("b15="+b15);
+// console.log("b16="+b16);
+// console.log("b17="+b17);
+// console.log("b18="+b18);
+// console.log("b19="+b19);
 
 
 
